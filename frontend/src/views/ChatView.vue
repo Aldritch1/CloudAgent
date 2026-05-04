237ef11b0d205c573f35e446af2b4e6c6d5d1edd
zhoumoyu <1271576898@qq.com>
2026-05-04 09:00:56 +0800

    feat: add Vue3 frontend scaffold

---
+++ frontend/src/views/ChatView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="chat-view">
+    <h1>CloudAgent Chat</h1>
+  </div>
+</template>
+
+<style scoped>
+.chat-view {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
